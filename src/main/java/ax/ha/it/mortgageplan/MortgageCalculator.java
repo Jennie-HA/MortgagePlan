0cea6c962f45bdb87f8546f858b9f6362d404cd0
Jennie Eriksson <jennie.eriksson89@gmail.com>
2021-02-23 21:59:19 +0200

    created main-file

---
+++ src/main/java/ax/ha/it/mortgageplan/MortgageCalculator.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ax.ha.it.mortgageplan;
+
+/**
+ *
+ * @author jennie
+ */
+public class MortgageCalculator {
+    public static void main(String arg[]) {
+        
+    }
+
+    
+}
