37dfb2400f128cd11dd503c85ada3e729fa6cef6
Jennie Eriksson <jennie.eriksson89@gmail.com>
2021-02-24 13:18:07 +0200

    reformatted data from file with regex

--- src/main/java/ax/ha/it/mortgageplan/MortgageCalculator.java
+++ src/main/java/ax/ha/it/mortgageplan/MortgageCalculator.java
@@ -5,14 +5,79 @@
  */
 package ax.ha.it.mortgageplan;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+
+
 /**
  *
  * @author jennie
  */
 public class MortgageCalculator {
+//    Your task is to create a Java application that reads the attached file and 
+//            print out the result for each customer given the formula below.
+
+//A build tool used to compile, build, test and run project
+//Readme of how to start the application
+//Unit test for testing application logic
+//Usage of java.math or similar math dependencies are not allowed
+    
     public static void main(String arg[]) {
+        BufferedReader br = null;
+       
+        try {
+            br = new BufferedReader(new FileReader("prospects.txt"));
         
-    }
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+                String reg = "\"(.+),(.+)\"";
+                String replacement = "$1 $2";
+                    if(line != null) {
+                        if (Pattern.compile(reg).matcher(line).find()) {
+                            line = line.replaceAll(reg, replacement); 
+                        }
+                        String[] array = line.split(",");
+                        for(int i = 0; i < array.length; i++) {
+                           System.out.println(array[i]);
+                        }
+                    }
+                
+                
+                                
+                
+    
+                
+            }
+            String data = sb.toString();
+            //System.out.println(data);
+    
+        } catch(FileNotFoundException ex){
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            if(br != null) {
+                try {
+                    br.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            
+        }
 
     
-}
+    }
+    
+}   
